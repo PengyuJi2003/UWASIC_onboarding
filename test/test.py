ed2e2f24e6ece5232bc8f429b62f6e5fb408903a
Joe Ji <jipengyu2003@163.com>
2025-10-15 14:41:22 -0400

    Edited PWM freq test8

--- test/test.py
+++ test/test.py
@@ -85,19 +85,19 @@ async def send_spi_transaction(dut, r_w, address, data):
     await ClockCycles(dut.clk, 600)
     return ui_in_logicarray(ncs, bit, sclk)
 
-async def measure_pwm_frequency(bus, bit) -> float:
-    '''
-    Measure the frequency (in Hz) of bus[bit]
-    '''
-    await RisingEdge(bus[bit])
-    t1 = get_sim_time(units='ns')
+# async def measure_pwm_frequency(bus, bit) -> float:
+#     '''
+#     Measure the frequency (in Hz) of bus[bit]
+#     '''
+#     await RisingEdge(bus[bit])
+#     t1 = get_sim_time(units='ns')
 
-    await RisingEdge(bus[bit])
-    t2 = get_sim_time(units='ns')
+#     await RisingEdge(bus[bit])
+#     t2 = get_sim_time(units='ns')
 
-    period_ns = t2 - t1
-    freq_hz = 1e9 / period_ns
-    return freq_hz
+#     period_ns = t2 - t1
+#     freq_hz = 1e9 / period_ns
+#     return freq_hz
 
 @cocotb.test()
 async def test_spi(dut):
@@ -212,9 +212,20 @@ async def test_pwm_freq(dut):
     ui_in_val = await send_spi_transaction(dut, 1, 0x04, 0x80)
     await ClockCycles(dut.clk, 10000)
 
-    freq1 = await measure_pwm_frequency(dut.uo_out, 0)
+    # freq1 = await measure_pwm_frequency(dut.uo_out, 0)
+    await RisingEdge([dut.uo_out[0]])
+    t1 = get_sim_time(units='ns')
+
+    await RisingEdge([dut.uo_out[0]])
+    t2 = get_sim_time(units='ns')
+
+    period_ns = t2 - t1
+    freq1 = 1e9 / period_ns
+
     assert (freq1 >= 2970 and freq1 <= 3030), f"Expected 3000 Hz, got {freq1} Hz"
     await ClockCycles(dut.clk, 20000)
+
+    
 
     # freq2 = await measure_pwm_frequency(dut.uio_out, 0)
     # assert (freq2 >= 2970 and freq2 <= 3030), f"Expected 3000Hz, got {freq2}"
